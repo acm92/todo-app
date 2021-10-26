59b95b1f57cf0bfd2b260910549f302f3045661e
angelcm92 <angelcld7@gmail.com>
2021-10-26 12:19:09 +0200

    Added Header, Footer and Routers

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,11 +1,26 @@
+import { render } from "@testing-library/react"
 import React, { Component } from "react"
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
+
+
 
 class TodoApp extends Component{
     render() {
         return(
             <div className="TodoApp">
-                My Todo Application
-                <LoginComponent />
+                <Router>
+                    <div>
+                        <HeaderComponent />
+                        <Switch>
+                            <Route path="/" exact component={LoginComponent} />
+                            <Route path="/login" component={LoginComponent} />
+                            <Route path="/welcome/:name" component={WelcomeComponent} />
+                            <Route path="/todos" component={ListToDosComponent} />
+                            <Route component={ErrorComponent}/>
+                        </Switch>
+                        <FooterComponent />
+                    </div>
+                </Router>
             </div>
         )
     }
@@ -40,8 +55,9 @@ class LoginComponent extends Component{
 
     loginClicked = () => {
         if(this.state.username === "angel" && this.state.password === "residentevil5"){
-            this.setState({ validCredentials: true })
-            this.setState({ invalidCredentials: false })
+            
+            this.props.history.push(`/welcome/${this.state.username}`)
+   
         } else {
             this.setState({ invalidCredentials: true })
             this.setState({ validCredentials: false })
@@ -60,6 +76,91 @@ class LoginComponent extends Component{
         )
     }
 }
+
+
+
+class WelcomeComponent extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>Welcome {this.props.match.params.name}</p>
+                <p>Manage your ToDo's <Link to="/todos">here</Link></p>
+            </div>
+        )
+    }
+}
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <div>
+                Header  <hr />
+            </div>
+        )
+    }
+}
+
+class FooterComponent extends Component {
+    render() {
+        return (
+            <div>
+                <hr />Footer
+            </div>
+        )
+    }
+}
+
+class ListToDosComponent extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state ={
+            todos: 
+            [
+                { id: 1, description: "Buy milk", done:false, targetDate: new Date() },
+                { id: 2, description: "Do sport", done: false, targetDate: new Date() },
+                { id: 3, description: "Coding", done: false, targetDate: new Date() }
+            ]
+        }
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>List Todos</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>description</th>
+                            <th>isCompleted?</th>
+                            <th>Target Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.todos.map((todo) => 
+                            <tr>
+                                <td>{todo.id}</td>
+                                <td>{todo.description}</td>
+                                <td>{todo.done.toString()}</td>
+                                <td>{todo.targetDate.toString()}</td>
+                            </tr>
+                        )} 
+                    </tbody>
+                </table>
+            </div>)
+    }
+
+}
+
+
+const ErrorComponent = () => (
+    <div>Error! Missing page! Contact support</div>
+)
+
+
 
 
 export default TodoApp
